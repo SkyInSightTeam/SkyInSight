66859cb3613739cb695c8825ad01fa29476f2957
Florian Lambert <flo2009@laposte.net>
2024-03-11 14:28:46 +0100

    🐱‍👤Code fonctionne, a tester, City api fonctionne🐱‍👤

--- src/City/City.cpp
+++ src/City/City.cpp
@@ -1,13 +1,17 @@
 #include "City.hpp"
-
+#include "../WeatherApiCaller/WeatherApiCaller.h"
 using namespace std;
+
 
 City::City(string lon, string lat, string displayName) {
     this->lon = lon;
     this->lat = lat;
     this->displayName = displayName;
 }
-
+size_t WriteCallback2(char* buf, size_t size, size_t nmemb, std::string* response) {
+    response->append(buf, size * nmemb);
+    return size * nmemb;
+}
 void City::getAllCitiesFromCityName(string cityName) {
     CURL* curl;
     CURLcode res;
@@ -15,7 +19,7 @@ void City::getAllCitiesFromCityName(string cityName) {
     curl = curl_easy_init();
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, "https://nominatim.openstreetmap.org/search?q=" + cityName + "&format=json");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback2);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
@@ -25,3 +29,4 @@ void City::getAllCitiesFromCityName(string cityName) {
     }
     cout << response;
 }
+
