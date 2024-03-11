cddc056564cbcb1f7daf136d32774d1e892df666
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2024-03-11 14:42:15 +0100

    😒 API Call working, but response empty 😒

--- src/City/City.cpp
+++ src/City/City.cpp
@@ -8,25 +8,24 @@ City::City(string lon, string lat, string displayName) {
     this->lat = lat;
     this->displayName = displayName;
 }
+
 size_t WriteCallback2(char* buf, size_t size, size_t nmemb, std::string* response) {
     response->append(buf, size * nmemb);
     return size * nmemb;
 }
+
 void City::getAllCitiesFromCityName(string cityName) {
     CURL* curl;
     CURLcode res;
     std::string response;
     curl = curl_easy_init();
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://nominatim.openstreetmap.org/search?q=" + cityName + "&format=json");
+        std::string req = "https://nominatim.openstreetmap.org/search?q=" + cityName + "&format=json";
+        curl_easy_setopt(curl, CURLOPT_URL, req);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback2);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
-        if (res == CURLE_OK) {
-            cout << response;
-        } 
     }
-    cout << response;
+    cout << "Réponse de l'API : " << response << endl;
 }
-
